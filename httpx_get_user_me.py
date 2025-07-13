948ce10b2b435b678f1a5a2596cb8d423b8f4746
SuperPracion <superpracion2@gmail.com>
2025-07-13 11:56:46 +0300

    Введение в HTTPX. Практическое задание: работа с HTTPX

---
+++ httpx_get_user_me.py
@@ -0,0 +1,16 @@
+import httpx
+
+login_body = {
+    "email": "superpracion@example.com",
+    "password": "superpracion",
+}
+login_response = httpx.post("http://localhost:8000/api/v1/authentication/login", json=login_body)
+
+
+token = login_response.json()["token"]["accessToken"]
+headers = {
+    "Authorization": f"Bearer {token}",
+}
+me_response = httpx.get("http://localhost:8000/api/v1/users/me", headers=headers)
+print(me_response.json())
+print(me_response.status_code)
