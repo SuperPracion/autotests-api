dc9ca058b6fb5d433c794dcc62c27b633cbfd19a
SuperPracion <superpracion2@gmail.com>
2025-07-15 20:15:25 +0300

    Знакомство с Pydantic. Практика написания Pydantic-моделей

---
+++ clients/pydantic_create_user.py
@@ -0,0 +1,34 @@
+from pydantic import BaseModel, Field, EmailStr, ConfigDict
+
+
+class UserSchema(BaseModel):
+    """
+    Модель структуры пользователя
+    """
+    model_config = ConfigDict(populate_by_name=True)
+
+    id: str
+    email: EmailStr
+    last_name: str = Field(alias="lastName")
+    first_name: str = Field(alias="firstName")
+    middleName: str = Field(alias="middleName")
+
+
+class CreateUserRequestSchema(BaseModel):
+    """
+    Модель структуры запроса на создание пользователя
+    """
+    model_config = ConfigDict(populate_by_name=True)
+
+    email: EmailStr
+    password: str
+    last_name: str = Field(alias="lastName")
+    first_name: str = Field(alias="firstName")
+    middleName: str = Field(alias="middleName")
+
+
+class CreateUserResponseSchema(BaseModel):
+    """
+    Модель структуры ответа создания пользователя
+    """
+    user: UserSchema
