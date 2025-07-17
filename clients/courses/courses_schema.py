f993989dc95389999e5fec5650aed426a35247f4
SuperPracion <superpracion2@gmail.com>
2025-07-17 16:08:26 +0300

    Знакомство с Faker. Практика работы с генерацией случайных данных

--- clients/courses/courses_schema.py
+++ clients/courses/courses_schema.py
@@ -2,6 +2,7 @@ from pydantic import BaseModel, Field, ConfigDict
 
 from clients.files.files_schema import FileSchema
 from clients.pydantic_create_user import UserSchema
+from tools.fakers import fake
 
 
 class CourseSchema(BaseModel):
@@ -26,13 +27,13 @@ class CreateCourseRequestSchema(BaseModel):
     """
     model_config = ConfigDict(populate_by_name=True)
 
-    title: str
-    max_score: int = Field(alias="maxScore")
-    min_score: int = Field(alias="minScore")
-    description: str
-    estimated_time: str = Field(alias="estimatedTime")
-    preview_file_id: str = Field(alias="previewFileId")
-    created_by_user_id: str = Field(alias="createdByUserId")
+    title: str = Field(default_factory=fake.sentence)
+    max_score: int = Field(alias="maxScore", default_factory=fake.max_score)
+    min_score: int = Field(alias="minScore", default_factory=fake.min_score)
+    description: str = Field(default_factory=fake.text)
+    estimated_time: str = Field(alias="estimatedTime", default_factory=fake.estimated_time)
+    preview_file_id: str = Field(alias="previewFileId", default_factory=fake.uuid4)
+    created_by_user_id: str = Field(alias="createdByUserId", default_factory=fake.uuid4)
 
 
 class CreateCourseResponseSchema(BaseModel):
