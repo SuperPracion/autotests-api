06a33f1441bfc390400cdcb74c598fc5d95ed126
SuperPracion <superpracion2@gmail.com>
2025-07-18 19:17:35 +0300

    Параметризация Pytest. Практика работы с pytest параметризацией

--- tools/assertions/users.py
+++ tools/assertions/users.py
@@ -1,4 +1,4 @@
-from clients.users.users_schema import UserSchema
+from clients.users.users_schema import CreateUserRequestSchema, CreateUserResponseSchema, UserSchema
 from tools.assertions.base import assert_equal
 
 
@@ -24,3 +24,17 @@ def assert_get_user_response(get_user_response, create_user_response):
     :raises create_user_response: ответ API при создании пользователя.
     """
     assert_user
+
+
+def assert_create_user_response(request: CreateUserRequestSchema, response: CreateUserResponseSchema):
+    """
+    Проверяет, что ответ на создание пользователя соответствует запросу.
+
+    :param request: Исходный запрос на создание пользователя.
+    :param response: Ответ API с данными пользователя.
+    :raises AssertionError: Если хотя бы одно поле не совпадает.
+    """
+    assert_equal(response.user.email, request.email, "email")
+    assert_equal(response.user.last_name, request.last_name, "last_name")
+    assert_equal(response.user.first_name, request.first_name, "first_name")
+    assert_equal(response.user.middle_name, request.middle_name, "middle_name")
