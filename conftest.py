43f585b8819b7b0ff18dc011b285219b4585d63b
SuperPracion <SuperPracion@users.noreply.github.com>
2025-08-04 08:43:48 +0000

    deploy: 349e9be49bede69b55f2c9b811240252973d87ae

--- conftest.py
+++
@@ -1,9 +0,0 @@
-pytest_plugins = (
-    "fixtures.users",
-    "fixtures.files",
-    "fixtures.courses",
-    "fixtures.authentication",
-    "fixtures.exercises",
-    "fixtures.exercises",
-    "fixtures.allure"
-)
